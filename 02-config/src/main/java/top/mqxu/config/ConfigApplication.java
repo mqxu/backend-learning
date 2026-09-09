52f2eee570bc6114354b3412febc5aa6c55343d6
mqxu <moqi1977@gmail.com>
2026-09-09 10:50:33 +0800

    feat: 完成 02-config 模块基本的配置属性读取

---
+++ 02-config/src/main/java/top/mqxu/config/ConfigApplication.java
@@ -0,0 +1,16 @@
+package top.mqxu.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mqxu
+ * @date 2026/9/9
+ * @description ConfigApplication
+ **/
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
---
+++ 02-config/src/main/java/top/mqxu/config/controller/ConfigController.java
@@ -0,0 +1,42 @@
+package top.mqxu.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mqxu
+ * @date 2026/9/9
+ * @description ConfigController
+ **/
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Value("${server.port}")
+    private Integer serverPort;
+
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+
+    @Value("${mqxu.name}")
+    private String myName;
+
+    @Value("${mqxu.job}")
+    private String myJob;
+
+
+    @GetMapping("/basic")
+    public String getBasicInfo() {
+        return "服务器端口是：" + this.serverPort + ",应用名称是：" + appName;
+    }
+
+
+    @GetMapping("/my")
+    public String getMyInfo() {
+        return "我的姓名是：" + this.myJob + ",职业是：：" + myJob;
+    }
+}
